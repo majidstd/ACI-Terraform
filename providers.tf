36b2ca2d0ee8f9b495593d07bd37b9f2225de8ba
Majid S <m.sotoodeh@gmail.com>
2021-07-19 16:01:11 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,9 +11,9 @@ terraform {
 provider "aci" {
   #version = "0.3.4"
   # Cisco ACI user name
-  username = apic_username
+  username = apic_user
   # Cisco ACI password
-  password = apic_password
+  password = apic_pass
   # Cisco ACI URL
   url      = apic_host
   insecure = true
