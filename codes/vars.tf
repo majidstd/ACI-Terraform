34286f0599e5303358aa4e56a5ad672a53d2991b
Majid S <m.sotoodeh@gmail.com>
2021-07-19 14:12:59 -0700

    Create vars.tf

---
+++ codes/vars.tf
@@ -0,0 +1,15 @@
+variable "apic_ip" {
+  type = string
+  description = "Hostname or IP address of your APIC controller" 
+}
+
+variable "aci_user" {
+  type = string
+  description = "Username for vsphere"
+}
+
+variable "aci_password" {
+  type = string
+  description = "The password for vsphere"
+}
+
