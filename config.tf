92c30c7005ee07149a88f97bce37396a51d3335c
Majid S <m.sotoodeh@gmail.com>
2021-07-20 16:56:30 -0700

    Update config.tf

--- config.tf
+++ config.tf
@@ -54,7 +54,7 @@ resource "aci_application_epg" "web" {
 resource "aci_epg_to_domain" "web" {
 
   application_epg_dn    = "${aci_application_epg.web.id}"
-  tdn                   = "${var.vmm_vcenter.id}"
+  tdn                   = "${var.vmm_vcenter}"
   binding_type          = "dynamicBinding"
   res_imedcy            = "immediate"
   instr_imedcy          = "immediate"
